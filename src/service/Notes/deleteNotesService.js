d8d68da98d7aa4c3e9e03a1161dd46fdcb5cb8ea
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-04-16 21:33:49 +0530

    fix: card-delete-button

--- src/service/Notes/deleteNotesService.js
+++ src/service/Notes/deleteNotesService.js
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const deleteNoteService = async (currentNote, tokenValue) => {
   try {
-    const response = await axios.delete(
-      `/api/notes/${currentNote._id}`,
-      { note: { ...currentNote } },
-      { headers: { authorization: tokenValue } }
-    );
+    const response = await axios.delete(`/api/notes/${currentNote._id}`, {
+      headers: { authorization: tokenValue },
+    });
     console.log(response);
     return response;
   } catch (error) {
